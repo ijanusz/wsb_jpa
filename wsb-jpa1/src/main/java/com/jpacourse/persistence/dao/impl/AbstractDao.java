4435798d8be8c9dc436abba72b6cfbb2cd19aeae
Janusz Ignaszak <ignaszakjanusz@gmail.com>
2024-12-06 15:27:05 +0100

    Migrate project

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/AbstractDao.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/AbstractDao.java
@@ -4,11 +4,11 @@ import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
 
 import com.jpacourse.persistence.dao.Dao;
 import org.springframework.transaction.annotation.Transactional;
